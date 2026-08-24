144c858d8321c8afa816fa24ddfc9193ad9d0a68
abdulrahmanamer1 <abdulrahman.a.alani1@gmail.com>
2026-08-24 14:44:33 +0300

    Delete static directory

--- static/v10.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  window.openHikmaMenu=function(){
-    const d=document.getElementById('h10Drawer'); if(d)d.classList.add('open');
-  };
-  window.closeHikmaMenu=function(){
-    const d=document.getElementById('h10Drawer'); if(d)d.classList.remove('open');
-  };
-  document.addEventListener('keydown',e=>{if(e.key==='Escape')window.closeHikmaMenu()});
-})();
